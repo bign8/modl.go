4d71e54f50ef8ec8cd209c67d682af4788cc4bb3
Nate Woods <big.nate.w@gmail.com>
2020-09-27 09:17:00 -0500

    start(modl.go): Lets try this out :tada:

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bign8/modl.go
+
+go 1.14
---
+++ modl.go
@@ -0,0 +1,4 @@
+// Package modl implements encoding and decoding of MODL as defined in modl.uk.
+package modl
+
+// TODO: everything :cry:
