0ffc0c713766e98f542bbd55a8ba0c4237d591ec
Nate Woods <big.nate.w@gmail.com>
2020-09-27 13:23:43 -0500

    fix(test): Actually do the output tests :white_check_mark:

--- modl.go
+++ modl.go
@@ -1,4 +1,10 @@
 // Package modl implements encoding and decoding of MODL as defined in modl.uk.
 package modl
 
-// TODO: everything :cry:
+import "github.com/bign8/modl.go/internal/fs"
+
+// Unmarshal parses the MODL-encoded data and stores the result
+// in the value pointed to by v.
+func Unmarshal(data []byte, v interface{}, files fs.FS) error {
+	return nil // TODO: everything :cry:
+}
--- modl_test.go
+++ modl_test.go
@@ -7,6 +7,13 @@ import (
 	"testing"
 )
 
+// Going with allow list given the number of failures while starting...
+// will likely reverse to a block list once something starts working :cry:
+var allow = map[string]bool{
+	"361": true,
+	"362": true,
+}
+
 type grammarTest struct {
 	ID       string   `json:"id"`
 	Input    string   `json:"input"`
@@ -23,13 +30,59 @@ func (test grammarTest) Name() string {
 }
 
 func (test grammarTest) Test(t *testing.T) {
-	t.Logf("Parsing %q", test.Input)
+	var (
+		array = []interface{}{}
+		class = map[string]interface{}{}
+		exp   = []byte(test.Expected)
+	)
+	if err := json.Unmarshal(exp, &array); err == nil {
+		exp, err := json.Marshal(array)
+		if err != nil {
+			t.Fatalf("Unable to re-serialize result: %q", err)
+		}
+		test.Expected = string(exp)
+		test.TestArray(t)
+	} else if err = json.Unmarshal(exp, &class); err == nil {
+		exp, err := json.Marshal(&class)
+		if err != nil {
+			t.Fatalf("Unable to re-serialize result: %q", err)
+		}
+		test.Expected = string(exp)
+		test.TestClass(t)
+	} else {
+		t.Fatalf("Unable to derive type of result: %q", test.Expected)
+	}
+}
+
+func (test grammarTest) TestArray(t *testing.T) {
+	subject := []interface{}{}
+	if err := Unmarshal([]byte(test.Input), &subject, nil); err != nil {
+		t.Fatalf("Unable to unmarshal modl %q: %q", test.Input, err)
+	}
+	bits, err := json.Marshal(&subject)
+	if err != nil {
+		t.Fatalf("Unable to json.Marshal response: %q: %q", subject, err)
+	}
+	if string(bits) != string(test.Expected) {
+		t.Fatalf("Failed Match\nExp: %q\nGot: %q", string(test.Expected), string(bits))
+	}
+}
+
+func (test grammarTest) TestClass(t *testing.T) {
+	subject := map[string]interface{}{}
+	if err := Unmarshal([]byte(test.Input), &subject, nil); err != nil {
+		t.Fatalf("Unable to unmarshal modl %q: %q", test.Input, err)
+	}
+	bits, err := json.Marshal(&subject)
+	if err != nil {
+		t.Fatalf("Unable to json.Marshal response: %q: %q", subject, err)
+	}
+	if string(bits) != string(test.Expected) {
+		t.Fatalf("Failed Match\nExp: %q\nGot: %q", string(test.Expected), string(bits))
+	}
 }
 
 func TestGrammar(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
 	jsonTests, err := ioutil.ReadFile("grammar/tests/base_tests.json")
 	if err != nil {
 		t.Skip("Cannot read base_tests.json (git submodule init): " + err.Error())
@@ -39,6 +92,12 @@ func TestGrammar(t *testing.T) {
 		t.Fatal("Unable to deserialize tests: " + err.Error())
 	}
 	for _, test := range tests {
+		if test.Expected == "DELETED" {
+			continue
+		}
+		if testing.Short() && !allow[test.ID] {
+			continue
+		}
 		t.Run(test.Name(), test.Test)
 	}
 }
