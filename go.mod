b28cf677cfbe879e8fa9a2002c9ab636f434e180
Nate Woods <big.nate.w@gmail.com>
2020-09-27 23:57:21 -0500

    dep(antlr): Use OTS parser generator :heavy_plus_sign:

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bign8/modl.go
 
 go 1.14
+
+require github.com/antlr/antlr4 v0.0.0-20200915201312-e73f72be7355
--- modl.go
+++ modl.go
@@ -1,10 +1,31 @@
 // Package modl implements encoding and decoding of MODL as defined in modl.uk.
 package modl
 
-import "github.com/bign8/modl.go/internal/fs"
+import (
+	"fmt"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+
+	"github.com/bign8/modl.go/internal/fs"
+	"github.com/bign8/modl.go/internal/parser"
+)
+
+//go:generate java --version # assert java exists (sudo apt install default-jdk)
+//go:generate wget --no-clobber https://www.antlr.org/download/antlr-4.8-complete.jar
+//go:generate java -jar ./antlr-4.8-complete.jar -Dlanguage=Go -Xexact-output-dir -o internal/parser grammar/antlr4/MODLLexer.g4
+//go:generate rm internal/parser/MODLLexer.interp internal/parser/MODLLexer.tokens
 
 // Unmarshal parses the MODL-encoded data and stores the result
 // in the value pointed to by v.
 func Unmarshal(data []byte, v interface{}, files fs.FS) error {
+	is := antlr.NewInputStream(string(data))
+	lexer := parser.NewMODLLexer(is)
+	for {
+		t := lexer.NextToken()
+		if t.GetTokenType() == antlr.TokenEOF {
+			break
+		}
+		fmt.Printf("%s (%q)\n", lexer.SymbolicNames[t.GetTokenType()], t.GetText())
+	}
 	return nil // TODO: everything :cry:
 }
